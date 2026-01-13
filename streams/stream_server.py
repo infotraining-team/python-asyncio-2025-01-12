532ba27c9892e6aec74bc02d42cf5a24bf97ead2
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2026-01-13 14:09:34 +0100

    usage example

--- streams/stream_server.py
+++ streams/stream_server.py
@@ -13,8 +13,12 @@ logger = logging.getLogger("Streaming Server")
 async def fake_data_creator():
     for i in range(1, 10):
         yield (f"Generated some data {i}\n")
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.1)
 
 @app.get("/stream")
 async def stream():
     return StreamingResponse(fake_data_creator(), media_type = "text/plain")
+
+## Start server:
+# uv run uvicorn stream_server:app --reload
+# http://127.0.0.1:8000/docs
